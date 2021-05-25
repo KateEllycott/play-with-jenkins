7b745d7a5050c756651c06cfbf83bbced871a91a
Ekaterina Sivakova <brannaminskugga@gmail.com>
2021-05-25 14:54:02 +0300

    Initial commit

---
+++ src/main/java/com/kateellycott/playwithjenkins/PlayWithJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.kateellycott.playwithjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayWithJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayWithJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kateellycott/playwithjenkins/PlayWithJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kateellycott.playwithjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayWithJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
